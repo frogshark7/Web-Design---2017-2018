3aae84b0b17b41da86005aafa37509bfa6937e03
frogshark7 <irfan.nafi716@gmail.com>
2018-03-16 23:12:13 -0400

    Implemented Fade Reveal Scroll
    3rd page adaptive to screen size, 2nd page 1st module is acting up, fixed loading bug from parallax

--- Code/JS/scroll_reveal.js
+++ Code/JS/scroll_reveal.js
@@ -1,11 +1,21 @@
 // GENERAL SETTING
-//window.sr = ScrollReveal({ reset: true });
+window.sr = ScrollReveal({ reset: true });
 
 // Custom Settings
+sr.reveal('.foo', { 
+  origin: 'right', 
+  duration: 2000 
+});
 
-//sr.reveal('.foo-1', { origin: 'right', duration: 2000 });
+sr.reveal('.foo-2', { 
+  rotate: { x: 100, y: 100, z: 0 },
+  duration: 1000
+});
 
-// JavaScript
-window.sr = ScrollReveal();
-sr.reveal('.foo');
-sr.reveal('.bar');
+sr.reveal('.foo-3', { 
+  viewFactor: 0.5
+});
+
+sr.reveal('.foo-4', { 
+  duration: 200 
+});
