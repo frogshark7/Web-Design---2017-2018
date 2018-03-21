5c4b0d6c6a769bb238228e2738d635a6e431cb38
frogshark7 <irfan.nafi716@gmail.com>
2018-03-20 21:20:54 -0400

    cc- nav changes

--- Code/JS/scroll_reveal.js
+++ Code/JS/scroll_reveal.js
@@ -6,16 +6,3 @@ sr.reveal('.foo', {
   origin: 'right', 
   duration: 2000 
 });
-
-sr.reveal('.foo-2', { 
-  rotate: { x: 100, y: 100, z: 0 },
-  duration: 1000
-});
-
-sr.reveal('.foo-3', { 
-  viewFactor: 0.5
-});
-
-sr.reveal('.foo-4', { 
-  duration: 200 
-});
