1e1b0f9185a36f9bf1674af708bdc108505c7917
frogshark7 <irfan.nafi716@gmail.com>
2018-05-23 22:33:41 -0400

    Fix

---
+++ Code/JS/common.js
@@ -0,0 +1,41 @@
+$(".h-menu-2 > ul.dropdown-menu").hover(function () {
+        $(".h-menu-2 > .dropdown-toggle").css("border-left", "5px solid #434343");
+    },
+    function () {
+        $(".h-menu-2 > .dropdown-toggle").css("border-left", "5px solid transparent");
+    });
+$(".h-menu-3 > ul.dropdown-menu").hover(function () {
+        $(".h-menu-3 > .dropdown-toggle").css("border-left", "5px solid #434343");
+    },
+    function () {
+        $(".h-menu-3 > .dropdown-toggle").css("border-left", "5px solid transparent");
+    });
+$(".h-menu-4 > ul.dropdown-menu").hover(function () {
+        $(".h-menu-4 > .dropdown-toggle").css("border-left", "5px solid #434343");
+    },
+    function () {
+        $(".h-menu-4 > .dropdown-toggle").css("border-left", "5px solid transparent");
+    });
+$(".h-menu-5 > ul.dropdown-menu").hover(function () {
+        $(".h-menu-5 > .dropdown-toggle").css("border-left", "5px solid #434343");
+    },
+    function () {
+        $(".h-menu-5 > .dropdown-toggle").css("border-left", "5px solid transparent");
+    });
+$(".h-menu-6 > ul.dropdown-menu").hover(function () {
+        $(".h-menu-6 > .dropdown-toggle").css("border-left", "5px solid #434343");
+    },
+    function () {
+        $(".h-menu-6 > .dropdown-toggle").css("border-left", "5px solid transparent");
+    });
+
+
+function openNav() {
+    document.getElementById("myNav").style.height = "50px";
+    document.getElementById("no-overflow").style.overflow = "hidden";
+}
+
+function closeNav() {
+    document.getElementById("myNav").style.height = "0px";
+    document.getElementById("no-overflow").style.overflow = "auto";
+}
